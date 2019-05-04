71180ba923e45c8f5610d93b3fda2f8df0592b1e
roytal <roytt0@gmail.com>
2019-05-04 14:31:45 +0300

    Update cbmcSorting_dirty_buffer.c

--- CBMC/cbmcSorting_dirty_buffer.c
+++ CBMC/cbmcSorting_dirty_buffer.c
@@ -1,12 +1,13 @@
 #include<string.h>
 #include<stdio.h>
 #include<stdlib.h>
+#include <assert.h>
 
 void isSorted( int * arr, int len)
 {	
 	for(int i = 0; i<len-1; i++)
 	{
-		assert(arr[i+1] >= arr[i]);
+		assert(arr[i+1] >= arr[i]);		
 	}
 	printf("Sorting is OK!"); 
 }
@@ -28,11 +29,6 @@ int isInside(int *arr, int x, int len)
 void isPermotation(int * original, int * copy , int len)
 {
 	int i, copy_len;
-// 	copy_len = sizeof(copy)/sizeof(int);
-	
-// 	assert(copy_len == len );
-	printf("the old and new arrays are from the same lenght"); 
-	
 	for (i =0; i<len; i++)
 	{
 		assert(isInside(copy, original[i], len));
@@ -41,25 +37,27 @@ void isPermotation(int * original, int * copy , int len)
 	printf("Permutation is OK!"); 
 }
 			
-	
-	
+void isBufferOverrun(int insertingIndex, int arrayLen)
+{
+	assert( insertingIndex < arrayLen )
+}
 	
 
 int main()
 {
-	int i, j, a, n, number[30];
-	
+	int i, j, a, n, number[10];
+	int sizeOfNumber = 10;
 	printf("Enter the value of N \n");
 	scanf("%d", &n);
-    	
+    
 	if (n <= 0)
 		return 0;
 	// malloc both array - orig and copy 
+	//int* number = malloc(n * sizeof(int));	
 	int* orig_number = malloc(n*sizeof(int));
 	
 	printf("Enter the numbers \n");
-	for (i = 0; i < n; ++i)
-		
+	for (i = 0; i < n; ++i)				
     {
         scanf("%d", &number[i]);
     }
@@ -67,6 +65,7 @@ int main()
 	// save the original input array
 	for (i = 0; i< n; i++)
 	{
+		isBufferOverrun(i, sizeOfNumber);
 		orig_number[i] = number[i];
 	}
 
@@ -83,29 +82,6 @@ int main()
 			}
 		}
 	}
-
-
-	/*  Assertions
-	assert that the array at the end is sorted
-	for (i = 0; i < n -1; i++)
-	{
-	    assert(number[i] < number[i+1]);
-	}
-	
-	//checking if the array is not sorted
-	//force klee to find if there is an input that canot be sorted by our procedure
-	if (!isSorted(number,n))
-	{
-	  printf("the array is not sorted\n");
-	  //klee_assert(0);
-	}
-	// assert that the array at the end is a permutation of the original array
-	if (!isPermotation(orig_number, number, n))
-	{
-	  printf("some numbers are missing\n");
-	  //klee_assert(0);
-	}
-	*/
 	
 	// printing the new sorted array 
 	printf("The numbers arranged in ascending order are given below \n");
