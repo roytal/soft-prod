cf41724555e005e2ab33cdfcbf41f86bd315597e
Elkana Gamliel <40407541+ElkanaGam@users.noreply.github.com>
2019-05-05 17:41:11 +0300

    Update sortingKlee_clean.c

--- KLEE/sortingKlee_clean.c
+++ KLEE/sortingKlee_clean.c
@@ -11,7 +11,7 @@ void isSorted( int * arr, int len)
 		if (arr[i+1] <arr[i])
 		{
 			//printf("There is a problem with sorting!\n"); 
-			//klee_assert(0);
+			klee_assert(0);
 		}
 	}
 	printf("Sorting is OK!\n"); 
@@ -39,7 +39,7 @@ void isPermotation(int * original, int * copy , int len)
 		if (!isInside(copy, original[i], len))
 		{
 			printf("There is a problem with permutation\n"); 
-			//klee_assert(0);
+			klee_assert(0);
 		}
 	}
 	printf("Permutation is OK!\n"); 
@@ -61,13 +61,9 @@ int main()
  	
 	//klee_make_symbolic(&n,sizeof(n),"n");
 	klee_make_symbolic(number, n*sizeof(int), "number");
-	//assume n ia a little number for avoiding malloc errors
-	//klee_assume(1<n  && n< 20);
-	// malloc both array - orig and copy 
-	//number = malloc(n * sizeof(int));	
+	
 	int* orig_number = malloc(n*sizeof(int));
 	
-	//printf("Enter the numbers \n");
 	// take numbres from user or set with klee
 // 	for (i = 0; i < n; ++i)
 // 	{
@@ -77,7 +73,7 @@ int main()
 
 	
 	// save the original input array
-	for (i = 0; i< n-2; i++)
+	for (i = 0; i< n; i++)
 	{
 		orig_number[i] = number[i];
 	}
@@ -96,38 +92,8 @@ int main()
 		}
 	}
 
-
-	/*  Assertions
-	assert that the array at the end is sorted
-	for (i = 0; i < n -1; i++)
-	{
-	assert(number[i] < number[i+1]);
-	}
 	
-
-	//checking if the array is not sorted
-	//force klee to find if there is an input that canot be sorted by our procedure
-	if (!isSorted(number,n))
-	{
-	  printf("the array is not sorted\n");
-	  //klee_assert(0);
-	}
-	// assert that the array at the end is a permutation of the original array
-	if (!isPermotation(orig_number, number, n))
-	{
-	  printf("some numbers are missing\n");
-	  //klee_assert(0);
-
-	}
-	*/
 	
-	// printing the new sorted array 
-	/*printf("The numbers arranged in ascending order are given below \n");
-	for (i = 0; i < n; ++i)
-	{
-		printf("here is %0x\n", number[i]);
-	}
-	*/
 	// Klee Assertions 
 	isPermotation(orig_number, number, n);
 	isSorted(number,n);
